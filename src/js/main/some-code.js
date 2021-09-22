2cc2f84a98d91e402220e166f4a02ba6328c14af
iammursal <iammursal@gmail.com>
2021-09-22 11:45:34 +0530

    js folder structure changed

--- src/js/main/some-code.js
+++ src/js/main/some-code.js
@@ -1,4 +1,12 @@
-document.addEventListener('click', function (event) {
-	if (!event.target.matches('#click-me')) return;
-	alert('You clicked me!');
-}, false);
+/* -------------------------------------------------------------------------- */
+/*                              CUSTOM JS STARTS                              */
+/* -------------------------------------------------------------------------- */
+document.addEventListener(
+  "click",
+  function (event) {
+    if (!event.target.matches("#click-me")) return;
+    alert("You clicked me!");
+  },
+  false
+);
+/* ----------------------------- CUSTOM JS ENDS ----------------------------- */
